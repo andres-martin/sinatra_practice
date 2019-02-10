93238391838772a6a122faf4a34bb20aed79c197
andres-martin <andresmartin266@gmail.com>
2019-02-09 21:05:03 -0500

    sinatra exercise one modified

--- solution.rb
+++ solution.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require 'erb'
 
 get '/' do
-    unless params[:nombre]
+    if params[:nombre].nil? || params[:nombre].empty?
         erb "<h1>Hola desconocido!</h1>"        
     else
-       erb "<h1>Hola #{params[:nombre]}!</h1>"
+        erb "<h1>Hola #{params[:nombre]}!</h1>"
     end
     #@nombre = params[:nombre]
     #erb :index
