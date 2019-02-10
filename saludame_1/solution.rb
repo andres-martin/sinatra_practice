bf04020dd77ee6b69eaac65378cc931864e3bce5
andres-martin <andresmartin266@gmail.com>
2019-02-09 20:52:40 -0500

    erb template modified

--- saludame_1/solution.rb
+++ saludame_1/solution.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require 'erb'
 
 get '/' do
     #unless params[:nombre]
-    #    "Hola desconocido!"        
+    #   erb "<h1>Hola desconocido!</h1>"        
     #else
-    #    "Hola #{params[:nombre]}!"
+    #   erb "<h1>Hola #{params[:nombre]}!</h1>"
     #end
     @nombre = params[:nombre]
     erb :index
---
+++ solution.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+get '/' do
+    unless params[:nombre]
+        erb "<h1>Hola desconocido!</h1>"        
+    else
+       erb "<h1>Hola #{params[:nombre]}!</h1>"
+    end
+    #@nombre = params[:nombre]
+    #erb :index
+end    
